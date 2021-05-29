68e4684511395e0fd0de47ff83ea45d5058fc2ee
katiewang22 <katie.wang1950f@gmail.com>
2021-05-28 21:14:22 -0400

    calm drawing time

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Draw, DrawRef } from "@benjeau/react-native-draw";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Draw
+      //ref={drawRef}
+      height={650}
+      width={300}
+      initialValues={{
+        color: "#000000",
+        thickness: 10,
+        paths: []
+      }}
+      brushPreview="none"
+      canvasStyle={{ elevation: 0, backgroundColor: "#ffffff" }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
